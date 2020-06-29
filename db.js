6eec1743969497c49307a8804b391c326ef3711c
sena986 <53561380+sena986@users.noreply.github.com>
2020-06-29 23:13:41 +0900

    mongodb install & dotenv

--- db.js
+++ db.js
@@ -1,54 +1,17 @@
-export const videos = [
-  {
-    id: 243422,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 23,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123222,
-      name: "Nico",
-      email: "NOO@emaal.com",
-    },
-  },
-  {
-    id: 111111,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 23,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123222,
-      name: "Nico",
-      email: "NOO@emaal.com",
-    },
-  },
-  {
-    id: 623235,
-    title: "Video sexy",
-    description: "This is something I love",
-    views: 23,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123222,
-      name: "Nico",
-      email: "NOO@emaal.com",
-    },
-  },
-  {
-    id: 142322,
-    title: "Video task",
-    description: "This is something I love",
-    views: 23,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123222,
-      name: "Nico",
-      email: "NOO@emaal.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handelError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handelError);
