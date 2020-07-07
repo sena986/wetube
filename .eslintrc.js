8e257936f8ef1af559b5b8e6641158aa3a580511
sena986 <53561380+sena986@users.noreply.github.com>
2020-07-08 01:54:27 +0900

    css playing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "off",
+    "object-shorthand": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
 };
